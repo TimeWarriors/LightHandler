bae4aca8491a7f83ecc97dc595669436e88be765
jt222ii <jt222ii@student.lnu.se>
2016-02-02 12:58:47 +0100

    Merge branch 'jonas'

--- app.js
+++ app.js
@@ -1,35 +1,47 @@
 'use strict';
 let Blink1 = require('node-blink1');
+let config = require('./config.json')
+let http = require('http');
 let blink1;
 
 
 try {
-    
+    console.log(config.userName);
     var date = new Date();
     var hours = date.getHours();
     var minutes = date.getMinutes();
     console.log(hours + " : " + minutes);
+    getHueLamps();
+    //changeColor("00:17:88:01:10:51:a6:fe-0b", 1000, 0, 255, 0); 
 
-    changeColor("20005E32", 1000, 0, 255, 0); 
-
-    setTimeout(() => {
-       changeColor("20005E32", 1000, 255, 255, 255); 
-    }, 2000);
-    setTimeout(() => {
-       changeColor("20005E32", 1000, 0, 0, 0); 
-    }, 4000);
-    setTimeout(() => {
-       turnOff("20005E32");
-    }, 5000);
-    setTimeout(() => {
-       warningFlash("20005E32");
-    }, 6000);
+//    setTimeout(() => {
+//       changeColor("20005E32", 1000, 255, 255, 255); 
+//    }, 2000);
+//    setTimeout(() => {
+//       changeColor("20005E32", 1000, 0, 0, 0); 
+//    }, 4000);
+//    setTimeout(() => {
+//       turnOff("20005E32");
+//    }, 5000);
+//    setTimeout(() => {
+//       warningFlash("20005E32");
+//    }, 6000);
     
 
 } catch(err) {
    console.log(err);  // might get this if your USB port is weird/flaky
 }
 
+function getHueLamps(){ //returnerar ingenting just nu
+    http.get(config.hueIp+"api/"+config.userName+"/lights", function(res) {
+          res.on('data', function (chunk) {
+            let object = JSON.parse(chunk);
+            console.log(object);
+          });
+    }).on('error', function(e) {
+      console.log("Got error: " + e.message);
+    });
+}
 
 function changeColor(lampId, millisecondsToChange, r, g, b){
     let blinkDevices = Blink1.devices();
@@ -41,7 +53,6 @@ function changeColor(lampId, millisecondsToChange, r, g, b){
     }
     else //här ska den köra med en else if mot philips hue istället
     {
-        console.log("ingen lampa");
         
     }
 }
